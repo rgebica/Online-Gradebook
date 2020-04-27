e658a485a7ae12223a9b3a546f961937caecea8f
Rafał <rgebica@gmail.com>
2020-04-27 20:50:04 +0200

    init project

---
+++ src/test/java/com/index/IndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.index;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
