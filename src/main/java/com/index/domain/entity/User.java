86dd6c051312ee27a22f2e817ec454a75d9d2f77
Rafał <rgebica@gmail.com>
2020-04-27 21:28:40 +0200

    Create project schema

--- src/main/java/com/index/domain/entity/User.java
+++ src/main/java/com/index/domain/entity/User.java
@@ -1,4 +1,14 @@
 package com.index.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long indexNumber;
+    private String firstName;
+    private String LastName;
 }
