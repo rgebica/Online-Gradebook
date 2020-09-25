fe4080f6799c1febb2f031539e728666200abe80
rgebica <rgebica@gmail.com>
2020-09-25 19:55:38 +0200

    added Account isEnable test

---
+++ src/test/java/com/index/AccountTest.java
@@ -0,0 +1,24 @@
+package com.index;
+
+import com.index.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AccountTest {
+
+    @Test
+    void isNotEnabled() {
+        User newUser = new User();
+        assertFalse(newUser.isEnabled());
+    }
+
+    @Test
+    void isEnabled() {
+        User newUser = new User();
+        newUser.setEnabled(true);
+        assertTrue(newUser.isEnabled());
+    }
+}
