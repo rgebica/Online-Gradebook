3cb69542106742f7f3698ec61fddc9df3c4d5a9f
Rafał <rgebica@gmail.com>
2020-12-06 22:49:50 +0100

    heroku deploy fixes

--- src/main/java/com/index/IndexApplication.java
+++ src/main/java/com/index/IndexApplication.java
@@ -3,13 +3,14 @@ package com.index;
 import com.index.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @Import(SwaggerConfiguration.class)
-public class IndexApplication {
+public class IndexApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(IndexApplication.class, args);
