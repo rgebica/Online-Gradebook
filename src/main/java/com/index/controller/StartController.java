60ec5357df245a6cce23ed8059104b6b24d87329
Rafał <rgebica@gmail.com>
2020-12-07 00:29:54 +0100

    fixed

---
+++ src/main/java/com/index/controller/StartController.java
@@ -0,0 +1,24 @@
+package com.index.controller;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RequestMapping("/")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@AllArgsConstructor
+public class StartController {
+
+    @CrossOrigin
+    @GetMapping
+    public ResponseEntity<String> starter() {
+        return new ResponseEntity<>("Starter Page", OK);
+    }
+}
