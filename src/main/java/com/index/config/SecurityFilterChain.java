e037abf96ece157b742cd74bfaa83034729d2ba6
Rafał <rgebica@gmail.com>
2020-12-10 17:43:06 +0100

    changes

---
+++ src/main/java/com/index/config/SecurityFilterChain.java
@@ -0,0 +1,17 @@
+package com.index.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityFilterChain extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+
+}
