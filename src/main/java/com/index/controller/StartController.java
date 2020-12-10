808183b6aed3e934d0f388b0b75050ca15843865
Rafał <rgebica@gmail.com>
2020-12-10 18:03:16 +0100

    changes

--- src/main/java/com/index/controller/StartController.java
+++ src/main/java/com/index/controller/StartController.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@RestController
 @RequestMapping(value = "")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @AllArgsConstructor
