0b5d2e5b1049b28a11e9b2ab24e133218a9cbcdf
Rafał <rgebica@gmail.com>
2020-12-10 17:38:52 +0100

    changes

--- src/main/java/com/index/IndexApplication.java
+++ src/main/java/com/index/IndexApplication.java
@@ -3,12 +3,13 @@ package com.index;
 import com.index.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
 @EnableAsync
 @Import(SwaggerConfiguration.class)
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class IndexApplication{
 
     public static void main(String[] args) {
