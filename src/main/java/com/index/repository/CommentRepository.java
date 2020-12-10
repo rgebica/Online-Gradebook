772e671a7fc5ed87cd67cbab2a9ac278d5727471
Rafał <rgebica@gmail.com>
2020-12-10 18:44:30 +0100

    comments deleted

--- src/main/java/com/index/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.index.repository;
-
-import com.index.model.Comments;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comments, Long> {
-
-}
